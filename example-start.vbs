308c356d79e97b38a75908a532a6df97cdaa49fb
Nick Meadows <meadowsnick98@gmail.com>
2024-02-26 13:17:28 -0600

    Add logging and kill off old process when new one starts

---
+++ example-start.vbs
@@ -0,0 +1,1 @@
+CreateObject("Wscript.Shell").Run "node C:\Users\meado\Github\Node-Red-MQTT-Listener\mqtt-start.js", 0
--- mqtt-start.js
+++ mqtt-start.js
@@ -1,8 +1,41 @@
 const mqtt = require("mqtt");
 var child_process = require('child_process');
-const path = require("path");
+const process = require('process');
 const config = require('./config.json');
 const Topic = require('./topicsEnum')
+const fs = require('fs')
+
+
+const writeToLog = (message) => {
+    const logMessage = new Date()+ ': '+ message+ '\n';
+    if(fs.existsSync('./log.txt')) {
+        fs.appendFileSync('./log.txt', logMessage);
+    } else {
+        fs.writeFileSync('./log.txt',logMessage);
+    }
+}
+
+
+const pidFilePath = "./pid"
+const pid = process.pid
+
+let pidFileContent;
+if(fs.existsSync(pidFilePath)) {
+    pidFileContent = fs.readFileSync(pidFilePath)
+}
+
+writeToLog('current pid file content: '+ pidFileContent)
+if(pidFileContent != undefined) {
+    writeToLog('killing pid '+pidFileContent)
+    child_process.exec('TASKKILL /F /PID '+pidFileContent, function (error, stdout, stderr) {
+        console.log('Killed processes', error, stdout, stderr);
+    });
+}
+
+pidFileContent = pid.toString();
+writeToLog('current pid '+pidFileContent);
+fs.writeFileSync(pidFilePath, pidFileContent);
+
 
 const client = mqtt.connect("mqtt://192.168.70.181:1883");
 client.on("connect", () => {
@@ -23,7 +56,7 @@ client.on("connect", () => {
 });
 
 client.on("message", (topic, message) => {
-    console.log('Got message', topic, message.toString())
+    writeToLog('Got message. topic: '+ topic+', message: '+message.toString())
     switch (topic) {
         case Topic.GamingPCDeskGaming: {
             const topicConfig = config.topics.gamingPCEnableDeskGaming;
@@ -47,18 +80,22 @@ client.on("message", (topic, message) => {
 });
 
 const loadMonitorConfig = (configFilePath) => {
+    writeToLog('loading monitor config file '+configFilePath);
     child_process.exec(`MultiMonitorTool /LoadConfig ${configFilePath}`, function (error, stdout, stderr) {
         console.log(stdout);
     });
 }
 const loadAudioConfig = (configFilePath) => {
+    writeToLog('loading audio config file '+configFilePath);
     child_process.exec(`soundvolumeview /LoadProfile  ${configFilePath}`, function (error, stdout, stderr) {
         console.log(stdout);
     });
 }
 
 const shutdownPC = () => {
+    writeToLog('shutting down Computer');
     child_process.exec('nirCmd exitwin shutdown', function (error, stdout, stderr) {
         console.log(stdout);
     });
 }
+
