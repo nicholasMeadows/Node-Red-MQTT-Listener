c823fcacf07ef61ec08108556a1f21db9e81f63d
Nick Meadows <meadowsnick98@gmail.com>
2024-02-26 14:32:10 -0600

    Add clean up log when script starts

--- mqtt-start.js
+++ mqtt-start.js
@@ -5,37 +5,70 @@ const config = require('./config.json');
 const Topic = require('./topicsEnum')
 const fs = require('fs')
 
+const logFilePath = "./log.txt"
+const pidFilePath = "./pid";
 
 const writeToLog = (message) => {
-    const logMessage = new Date()+ ': '+ message+ '\n';
-    if(fs.existsSync('./log.txt')) {
-        fs.appendFileSync('./log.txt', logMessage);
+    const now = new Date();
+    const dateString = `${now.getMonth()+1}-${now.getDate()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+    const logMessage = dateString+ ': '+ message+ '\n';
+    if(fs.existsSync(logFilePath)) {
+        fs.appendFileSync(logFilePath, logMessage);
     } else {
-        fs.writeFileSync('./log.txt',logMessage);
+        fs.writeFileSync(logFilePath,logMessage);
     }
+    console.log(logMessage);
 }
 
+const killOldProcess = () => {
+    const pid = process.pid
 
-const pidFilePath = "./pid"
-const pid = process.pid
-
-let pidFileContent;
-if(fs.existsSync(pidFilePath)) {
-    pidFileContent = fs.readFileSync(pidFilePath)
+    let pidFileContent;
+    if(fs.existsSync(pidFilePath)) {
+        pidFileContent = fs.readFileSync(pidFilePath)
+    }
+    
+    writeToLog('current pid file content: '+ pidFileContent)
+    if(pidFileContent != undefined) {
+        writeToLog('killing pid '+pidFileContent)
+        child_process.exec('TASKKILL /F /PID '+pidFileContent, function (error, stdout, stderr) {
+            console.log('Killed processes', error, stdout, stderr);
+        });
+    }
+    
+    pidFileContent = pid.toString();
+    writeToLog('current pid '+pidFileContent);
+    fs.writeFileSync(pidFilePath, pidFileContent);
 }
 
-writeToLog('current pid file content: '+ pidFileContent)
-if(pidFileContent != undefined) {
-    writeToLog('killing pid '+pidFileContent)
-    child_process.exec('TASKKILL /F /PID '+pidFileContent, function (error, stdout, stderr) {
-        console.log('Killed processes', error, stdout, stderr);
-    });
+const clearOldLogMessages = () => {
+    if(!fs.existsSync(logFilePath)) {
+        return;
+    }
+    const removeLogsAfterDays = 2;
+    // const removeLogsAfterDaysMilliseconds = removeLogsAfterDays*24*60*60*1000;
+    const removeLogsAfterDaysMilliseconds = 2*60*1000;
+
+    const logFileContent = fs.readFileSync(logFilePath, {encoding:'utf-8'});
+    const lines = logFileContent.split('\n');
+
+    const now = new Date().getTime();
+    let linesToWrite = lines.map(line => {
+        const dateString = line.split(': ')[0];
+        const date = new Date(dateString);
+        const timeMilliseconds = date.getTime();
+
+        if(timeMilliseconds > now - removeLogsAfterDaysMilliseconds) {
+            return line;
+        }
+        return '';
+    })
+    linesToWrite = linesToWrite.filter(line => line !== '');
+    fs.writeFileSync(logFilePath, linesToWrite.join('\n'));    
 }
 
-pidFileContent = pid.toString();
-writeToLog('current pid '+pidFileContent);
-fs.writeFileSync(pidFilePath, pidFileContent);
-
+killOldProcess();
+clearOldLogMessages();
 
 const client = mqtt.connect("mqtt://192.168.70.181:1883");
 client.on("connect", () => {
